20a9c0d0b4d81c9d9cb3b3e332581290adf4331d
a-t-0 <eai1@protonmail.com>
2019-11-17 21:29:10 +0100

    Converted MongoDB to array, inspectable per element in HTML.

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,48 @@
 import React, { Component } from 'react';
 import './App.css';
-
+var rebels;
 class App extends Component {
-  state = {
-    data: [],
-    id: 0,
-    message: null,
-    intervalIsSet: false,
-    idToDelete: null,
-    idToUpdate: null,
-    objectToUpdate: null,
-  };
-
-  componentDidMount() {
-    this.getDataFromDb();
-    if (!this.state.intervalIsSet) {
-      let interval = setInterval(this.getDataFromDb, 500);
-      this.setState({ intervalIsSet: interval });
+    state = {
+        data: [],
+        id: 0,
+        message: null,
+        intervalIsSet: false,
+        idToDelete: null,
+        idToUpdate: null,
+        objectToUpdate: null,
+    };
+    
+    componentDidMount() {
+        this.getDataFromDb();
+        if (!this.state.intervalIsSet) {
+            let interval = setInterval(this.getDataFromDb, 500);
+            this.setState({ intervalIsSet: interval });
+        }
     }
-  }
-
-  componentWillUnmount() {
-    if (this.state.intervalIsSet) {
-      clearInterval(this.state.intervalIsSet);
-      this.setState({ intervalIsSet: null });
+    
+    componentWillUnmount() {
+        if (this.state.intervalIsSet) {
+            clearInterval(this.state.intervalIsSet);
+            this.setState({ intervalIsSet: null });
+        }
     }
-  }
-
-  getDataFromDb = () => {
-    fetch('http://localhost:3001/api/getData')
-      .then((data) => data.json())
-      .then((res) => this.setState({ data: res.data }));
-  };
-
+    
+    getDataFromDb = () => {
+        fetch('http://localhost:3001/api/getData')
+                .then((data) => data.json())
+                .then((res) => this.setState({ data: res.data }));
+    };
+    
+    // This transforms the data object property temperature into an array!
+    // Source: https://medium.com/poka-techblog/simplify-your-javascript-use-map-reduce-and-filter-bd02c593cc2d
+    getSingleEntry(data) {
+        var officersIds = [];
+        data.forEach(function (data) {officersIds.push(data.temperature);})   
+        console.log(officersIds[2]);
+        return officersIds;
+    }
+    
+    
   render() {
     const { data } = this.state;
     return (
@@ -70,6 +80,17 @@ class App extends Component {
               ))}
           </tbody>   
         </table>
+		
+		dat.temperature={data.map((dat) => dat.temperature)}
+		<br></br>
+		dat.temperature={data.map((dat) => dat.temperature.length)}
+		<br></br>
+		dat.id={data.map((dat) => dat._id)}
+		
+                {/*Source: https://medium.com/poka-techblog/simplify-your-javascript-use-map-reduce-and-filter-bd02c593cc2d*/}
+		<br></br>
+		hi = {this.getSingleEntry(data)}
+                
       </div>
     );
   }
