e6abdec5575ff395efd3e11f22f5d2716bbc8422
a-t-0 <eai1@protonmail.com>
2019-12-23 17:12:09 +0100

    Swapped to faculty collection.

---
+++ backend/faculties.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Create another collection
+const FacultyCollection = new Schema(
+  {
+    name: String,
+	amountOfBuildings: Number
+  },
+  { collection: 'faculties' },
+  { timestamps: true ,useUnifiedTopology: true}
+);
+
+// export the new Schema so we could modify it using Node.js
+module.exports = mongoose.model("faculties", FacultyCollection);
